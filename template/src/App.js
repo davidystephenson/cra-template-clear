9f9f5102c79a90510025fe2e6edf69cf417f21c9
David Y. Stephenson <david@davidystephenson.com>
2022-07-14 11:06:37 +0200

    Use JavaScript

--- template/src/App.js
+++ template/src/App.js
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <>
       <h1>Hello</h1>
-
+      
       <p>World</p>
     </>
   )
--- template/src/index.js
+++ template/src/index.js
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-
 const container = document.getElementById('root')
-if (container == null) throw new Error('No root element found')
-const root = ReactDOM.createRoot(container)
+const root = ReactDOM.createRoot(containner)
 const app = (
   <React.StrictMode>
     <App />
